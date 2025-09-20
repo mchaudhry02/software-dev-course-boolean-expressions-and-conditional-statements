984dfccc6586de7c20c8c65b68d3144330bdfed5
mchaudhry02 <mchaudhry20@hotmail.com>
2025-09-20 12:49:27 -0500

    commit a boolean request in the game

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -26,21 +26,65 @@ Paste the following code into your editor:
 
 const readline = require('readline-sync');
 
-const hasTorch = true;
-const hasMap = false;
+let hasSword = false;
+let hasCompass = false;
 
+function startGame() {
+console.log("You wake up in a dark forest.");
 console.log("You see two paths: one leads to the mountains, the other to the village.");
 const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
 
-if (choice === "mountains" && hasTorch) {
-  console.log("You safely navigate through the dark mountains.");
-} else if (choice === "mountains" && !hasTorch) {
-  console.log("It's too dark to proceed. You decide to turn back.");
-} else if (choice === "village" || hasMap) {
-  console.log("You find your way to the village.");
+if (choice === "mountains") {
+  mountainPath();
+} else if (choice === "village") {
+  villagePath();
 } else {
-  console.log("You get lost and wander aimlessly.");
+  console.log("You get lost and wander aimlessly. Try again.");
+  startGame();
 }
+}
+
+function mountainPath(){
+  console.log("\n You head towards the mountains and find a sword stuck in a rock");
+  let answer = readline.question("Do you take the sword? (yes/no): ");
+    if(answer === "yes") {
+      hasSword = true;
+      console.log("You now have a sword!");
+    }
+    console.log("You continue climbing and face a wild bear!")
+
+    if (hasSword) {
+      console.log("You fight bravely with your sword and scare the bear away!");
+    } else {
+      console.log("You have no weapon and the bear chases you back down the mountain!");
+    }
+    endGame();
+
+}
+
+function villagePath() {
+  console.log("\n You Walk into a small village.");
+  console.log("A merchant offers you a compass.");
+  let answer = readline.question("Do you buy the compass? (yes/No) :");
+    if (answer === "yes") {
+      hasCompass = true;
+      console.log("you now have a compass!");
+    }
+    console.log("You hear rumor of treasure in the forest.");
+
+    if (hasCompass) {
+      console.log("With your compass, you easily navigate the forest and find the treasure chest!");
+      } else {
+        console.log("You Wander into the forest but get losr ...");
+      }
+      endGame();
+  }
+
+function endGame() {
+  console.log("\n---The End---");
+}
+
+startGame();
 
 /* 
 
